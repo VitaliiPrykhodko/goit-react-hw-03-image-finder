004d1c15131b58954af66f62e0af9776ed8f151e
VitaliiPrykhodko <vitaliimanager1@gmail.com>
2021-11-11 21:11:19 +0200

    запустив React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    pictures: [],
+    filter: "",
+  };
+  
+
+  handleFilter = (e) => {
+  };
+
+
+    render() {
+    return 
+  }
+}
+
+export default App;
